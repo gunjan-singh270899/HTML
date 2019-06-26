3f2c497ffdbf5d74c7dee80cc7fc8174429940e2
gunjan-singh270899 <51438410+gunjan-singh270899@users.noreply.github.com>
2019-06-26 13:43:43 +0530

    Add files via upload

---
+++ function.php
@@ -0,0 +1,11 @@
+<?php
+echo substr("Hello",4);
+echo strlen("hfkcjgs");
+echo trim("hi gunjan");
+echo strtolower("HHFUJFJKKOGDDC");
+$str="svhbkjhbxhk";
+echo $str;
+$var=gzcompress($str);
+echo $var;
+echo gzuncompress($var);
+?>
